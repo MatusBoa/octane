f381ab81a2eeeb1c7dce87145363b085482b9d8e
Matúš Koterba <matus.koterba@gmail.com>
2024-09-10 13:22:47 +0200

    Add keep-config option

--- src/Commands/InstallCommand.php
+++ src/Commands/InstallCommand.php
@@ -22,7 +22,8 @@ class InstallCommand extends Command
      * @var string
      */
     public $signature = 'octane:install
-                    {--server= : The server that should be used to serve the application}';
+                    {--server= : The server that should be used to serve the application}
+                    {--keep-config : Do not overwrite the existing configuration}';
 
     /**
      * The command's description.
@@ -53,7 +54,10 @@ class InstallCommand extends Command
             if ($installed) {
                 $this->updateEnvironmentFile($server);
 
-                $this->callSilent('vendor:publish', ['--tag' => 'octane-config', '--force' => true]);
+                $this->callSilent('vendor:publish', [
+                    '--tag' => 'octane-config',
+                    '--force' => ! $this->option('keep-config'),
+                ]);
 
                 $this->components->info('Octane installed successfully.');
                 $this->newLine();
